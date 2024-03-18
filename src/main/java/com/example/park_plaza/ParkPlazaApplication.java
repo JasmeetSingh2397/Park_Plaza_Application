db501d0e418a79ee516b8211cca13b52f5001935
Jasmeet Singh <jasmeetsingh@Jasmeets-MacBook-Air.local>
2024-03-18 18:20:41 +0530

    Implemented the Parking Lot facility

---
+++ src/main/java/com/example/park_plaza/ParkPlazaApplication.java
@@ -0,0 +1,13 @@
+package com.example.park_plaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkPlazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkPlazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/park_plaza/ParkPlazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.park_plaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkPlazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
